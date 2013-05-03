7f00db6a13972601e29baecafce0203673e7dc77
Carlos Antonio <billahill@gmail.com>
2013-05-03 14:17:15 +0200

    novos ficheiros

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.css" />
+        <link rel="stylesheet" href="css/sanguebom.css" />
+        <link rel="stylesheet" href="css/sanguebom.min.css" />
+        <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+        <script src="http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.js"></script>
+    </head>
+    <body>
+        <div data-role="header">
+            <h1>Sangue Bome</h1>
+        </div>
+    </body>
+</html>
